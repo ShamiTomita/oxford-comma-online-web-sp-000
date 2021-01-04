89c5b999d3d19353c8b89b5ec68917251f848ef4
Shami Tomita <shamitomita@gmail.com>
2021-01-04 19:15:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,6 @@ elsif array.count == 3
   new_array = array.join (", ")
   new_array.insert(14, "and ")
 else array.count > 3
-  array.last.insert( "and")
+  array.last.insert(last, "and")
 end 
 end
