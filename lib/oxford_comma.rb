5417f145f0c61a8b75b8bc9c280c803b1a7af978
Shami Tomita <shamitomita@gmail.com>
2021-01-04 18:59:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.count == 1
 elsif array.count == 2 
   array.join(" and ")
 elsif array.count == 3 
-  new_array = array.join (" , ")
+  new_array = array.join (", ")
   new_array.insert(2, "and")
 end 
 end
