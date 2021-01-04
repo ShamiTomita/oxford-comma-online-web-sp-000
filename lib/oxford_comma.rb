5bc5ae6523fcf5bbffc6ccae822ea68fc71f356e
Shami Tomita <shamitomita@gmail.com>
2021-01-04 18:57:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-if array.count == 2 
-  array.join(" and ")
-  elsif array.count < 2 
+if array.count < 2 
   return array 
+elsif array.count == 2 
+  array.join(" and ")
 elsif array.count == 3 
   new_array = array.join (" , ")
   new_array.insert(2, "and")
